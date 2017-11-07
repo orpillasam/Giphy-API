74751147ffea2e2c96879d0fe400a2b43abdc4ce
orpillasam <orpillasam@gmail.com>
2017-11-07 09:08:43 -0800

    index.html

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,49 +1,35 @@
 $(document).ready(function() {
 
-	var subjects = ['dogs', 'cats', 'birds'];
+	var subjects = ['Spock', 'Captain Kirk', 'Picard', 'Riker', 'Data', 'Janeway', 'Enterprise',
+					'Borg', 'Klingon', 'Ferengi', 'Sisko', 'Captain Archer', 'Uhura', 'Chekov', 'Scotty', 
+					'Sulu', 'Geordi', 'Troi',  "Miles O'Brien", 'Worf', 'Tuvok'];
 
-	renderButtons();
 	function renderButtons(){
-
 		$("#button-div").empty();
-
-        // Looping through the array of movies
         for (var i = 0; i < subjects.length; i++) {
-
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of movie to our button
-          a.addClass("subject");
-          // Adding a data-attribute
-          a.attr("data-name", subjects[i]);
-          // Providing the initial button text
-          a.text(subjects[i]);
-          // Adding the button to the buttons-view div
-          $("#button-div").append(a);
+			var a = $("<button>");
+			a.addClass("subject");
+			a.attr("data-name", subjects[i]);
+			a.text(subjects[i]);
+			$("#button-div").append(a);
         }
-      }
+    }
 
-      $('#add-subject').on('click', function(event){
-      		event.preventDefault();
-
-      		var subject = $('#subject-input').val().trim();
-
-      		subjects.push(subject);
-
-      		renderButtons();
-
-      });
+	$('#add-subject').on('click', function(event){
+			event.preventDefault();
+			var subject = $('#subject-input').val().trim();
+			subjects.push(subject);
+			renderButtons();
+    });
 
     $(document).on('click', ".subject", displayGifInfo);
 
 	function displayGifInfo() {
-	// $(".subject").on("click", function() {
-	// 	console.log('button was clicked');
 		var subject = $(this).attr('data-name');
+		console.log("subject is " + subject);
+
 		var queryURL ="http://api.giphy.com/v1/gifs/search?q=" + subject + "&api_key=56uYq7JLVc0ntzCICXjAnKxSDyNVuKyF&limit=10";
-		
-       		
+		 		
         $.ajax({
           url: queryURL,
           method: "GET"
@@ -52,49 +38,46 @@ $(document).ready(function() {
         	var results = response.data;
 
         	for (var i = 0; i < results.length; i++){
-
-
-	        	var gifDiv = $('<div class="gifWell">');
-
-
+	        	var gifDiv = $('<div class="gifs">');
 	        	var rating = response.data[i].rating;
 	        	console.log("rating is " + rating);
+	        	var ratingUpper = rating.toUpperCase();
+	        	var ratingDiv = $('<div>').text("Rating: " + ratingUpper);
+	        	var br = $('<br>');
+	        	if (rating === ""){
+	        		rating = 'Unrated';      		
+	         	}
 
-	        	var ratingDiv = $('<div>').text("Rating: " + rating);
-
-	        	var imgURL = response.data[i].images.original.url;
-	        	console.log("img url is " + imgURL);
-
-	        	gifDiv.append(rating);
-
-	        	var image = $('<img>').attr("src", imgURL);
-        		image.addClass('gif');
-        		image.attr('data-state', 'still');
-        		image.attr('data-still', response.data[i].images.original_still.url);
-        		image.attr('data-animate', response.data[i].images.original.url);
+	         	var image = $('<img>')
+	         	image.attr("src", results[i].images.fixed_height.url);
+	        	// image.attr(response.data[i].images.fixed_height.url;  
+        		image.attr('data-state', 'animate');
+        		image.attr('data-still', results[i].images.fixed_height_still.url);
+        		image.attr('data-animate', results[i].images.fixed_height.url);
+	        	gifDiv.append(ratingDiv);
+	        	gifDiv.append(br);
 	        	gifDiv.append(image);
-
+	        	image.addClass('gif');
 	        	$('#gifWell').prepend(gifDiv);
         	}
-
      	});
     }
 
-    $('.gif').on('click', function() {
+
+    $("body").on('click', '.gif', (function() {
     	console.log("gif button is clicked");
-
     	var state = $(this).attr('data-state');
-
+    	console.log('state is ' + state);
     	if (state === 'still'){
-    		$(this).attri('src', $(this).attr('data-animate'));
+    		$(this).attr('src', $(this).attr('data-animate'));
     		$(this).attr('data-state', 'animate');
     	} else {
     		$(this).attr('src', $(this).attr('data-still'));
     		$(this).attr('data-state', 'still');
     	}
-    });
-
-
+    	console.log('state is now ' + state);
+    }));
+    renderButtons();
 });
 
 
@@ -111,6 +94,3 @@ $(document).ready(function() {
 
 
 
-
-
-// });
